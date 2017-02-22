3aedbe0cb7766c2fc9e908700ec62b15730b6f6e
BexCTDev <developer@bexct.com>
2017-02-21 23:15:53 -0500

    Setup Basic Website template

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo ucwords(str_replace('-', ' ', basename(__DIR__)));?></title>
+</head>
+<body>
+<h1><?php echo ucwords(str_replace('-', ' ', basename(__DIR__)));?></h1>
+</body>
+</html>
